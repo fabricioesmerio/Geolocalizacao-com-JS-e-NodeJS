5ecdd473dbdc1ff226f852102b8e3bd89821fd38
Fabricio Esmerio <fabricio1esmerio@gmail.com>
2018-07-18 00:45:25 -0300

    Configuração servidor e criação do front

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const restify = require("restify");
 
+const googleMapsClient = require('@google/maps').createClient({
+    key: 'AIzaSyAsH77shal_ixB1_-o7FkKs8Hx8SCArPwo',
+    Promise: Promise
+});
+
 const knex = require("knex")({
     client: 'mysql',
     connection: {
@@ -21,13 +26,40 @@ server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
 
-server.get("/all", function(req, res, next){
+server.get("/all", function (req, res, next) {
     knex('places').then((dados) => {
         res.send(dados);
     }, next)
     return next();
 });
 
-server.listen(8080, function(){
+
+server.post("/geocode", function (req, res, next) {
+
+    const { lat, lng } = req.body
+
+    googleMapsClient.reverseGeocode({ latlng: [lat, lng] }).asPromise()
+        .then((response) => {
+            const address = response.json.results[0].formatted_address;
+            const place_id = response.json.results[0].place_id;
+            const image = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=300x300&sensor=false`;
+
+            knex('places')
+            .insert({place_id, address, image})
+            .then(() => {
+                res.send({address, image});
+            }, next)
+        })
+        .catch((err) => {
+            res.send(err);
+        });
+});
+
+server.get('/*/',restify.plugins.serveStatic({
+    directory: './dist',
+    default: 'index.html',
+}));
+
+server.listen(8080, function () {
     console.log("%s listening at %s", server.name, server.url);
 });
